117444eeddd3b6942260ee994755e86f902b3715
TowaInoue-kg <156703677+TowaInoue-kg@users.noreply.github.com>
2024-01-16 18:15:13 +0900

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,18 +1,17 @@
-# 必要なライブラリを読み込む機能(2024/1/16)
 require 'fileutils'
 
-# 対象のディレクトリパス指定する機能(2024/1/16)
-puts "Enter path"
+# 対象のフォルダのパスを指定する機能
+puts "Enter path:"
 directory_path = gets.chomp
 
-# コマンドライン上で指定のファイル名を尋ねる機能(2024/1/16)
+# コマンドライン上で指定のファイル名を尋ねる機能
 puts "Enter the base file name:"
 base_name = gets.chomp
 
-# ディレクトリ内のファイルを取得する機能(2024/1/16)
+# フォルダ内のデータを取得する機能
 files = Dir.entries(directory_path).select { |f| File.file? File.join(directory_path, f) }
 
-# ファイルを1から順番に番号＋指定の名前に変更する機能
+# データを1から順番に番号＋指定の名前に変更する機能
 files.each_with_index do |file, index|
   ext = File.extname(file)
   new_name = "#{index + 1}_#{base_name}#{ext}"
